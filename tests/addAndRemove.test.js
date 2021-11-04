f217a246e1eddb634e76932d04ce618cebe27483
mprotic123 <milan.protic@ts9maj.edu.rs>
2021-11-04 13:55:34 +0100

    Updated to check for list changes

---
+++ tests/addAndRemove.test.js
@@ -0,0 +1,45 @@
+
+
+ import trashTask from '../src/removeTask.js';
+ import addTask from '../src/addTask.js';
+ import { getStorage, saveStorage } from '../src/storage.js';
+ 
+ jest.mock('../src/storage.js');
+ jest.mock('../src/index.js');
+ const inputList = document.createElement('input');
+ inputList.type = 'text';
+ inputList.value = 'read a book';
+ 
+ describe('Testing the addTask function', () => {
+   test('test the function addList', () => {
+     saveStorage([]);
+     expect(addTask(inputList)).toEqual({
+       description: 'read a book',
+       completed: false,
+       index: 1,
+     });
+   });
+ 
+   test('Test adding and getting items from the storage ', () => {
+     const inputList = document.createElement('input');
+     inputList.type = 'text';
+     inputList.value = 'watch a movie';
+     saveStorage([]);
+     addTask(inputList);
+     expect(getStorage()).toEqual([{
+       description: 'watch a movie',
+       completed: false,
+       index: 1,
+     }]);
+   });
+ 
+   test('Test creating the elements "Dom" after adding new tasks', () => {
+     saveStorage([]);
+     addTask(inputList);
+     addTask(inputList);
+     addTask(inputList);
+     const items = Array.from(document.querySelectorAll('.list-item')).length;
+     expect(items).toBe(3);
+   });
+ });
+ 
